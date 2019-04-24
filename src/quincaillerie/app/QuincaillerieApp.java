1d4474b5f7cf7798cc04a9b75c2cd9b8ceeb43bb
Fares Belaid <belaid.fares1995@gmail.com>
2019-04-24 22:21:46 +0100

    all project

---
+++ src/quincaillerie/app/QuincaillerieApp.java
@@ -0,0 +1,17 @@
+
+package quincaillerie.app;
+
+
+public class QuincaillerieApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login_interface login=new login_interface();
+        //Nouveau_client_interface login=new Nouveau_client_interface();
+        login.show();
+        
+    }
+    
+}
